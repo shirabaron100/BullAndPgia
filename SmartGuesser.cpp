0be964d9571e10353b63d66daa0b833a3eacbd9b
shira baron <44798397+shirabaron100@users.noreply.github.com>
2019-05-06 13:01:43 +0300

    Add files via upload

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -7,13 +7,20 @@ string SmartGuesser::guess() {
 
      for (int i=0;i<10000;i++)
     {
+        //std::cout << "shira" << i  << std::endl;
         if (this->a[i]==true)
+        {
+        // std::cout << "index" << i  << std::endl;
         this->whatTOguess=SmartGuesser::convertIndexTOstr(i);
+         return this->whatTOguess;
+        }
+       // std::cout << " what" << this->whatTOguess.length() << std::endl;
     }
- return this->whatTOguess;
+
 }
 void SmartGuesser::startNewGame(uint length){  
     this->length=length;
+    //std::cout << "length" << this->length <<  std::endl;
     for (int i=0;i<10000;i++)
     {
         this->a[i]=true;
@@ -23,12 +30,13 @@ void SmartGuesser::learn(string reply){
     
     for (int i=0;i<10000;i++)
     {
-        
+        //std::cout << "i" << i << std::endl;
         string s=SmartGuesser::convertIndexTOstr(i);
-
+       if(s.compare("??")==0)
+       return;
         if (bullpgia::calculateBullAndPgia(s,whatTOguess).compare(reply)!=0)
         {
- // std::cout << "false" << std::endl;
+          //std::cout << "false" << std::endl;
             this->a[i]=false;
         }
     }
@@ -36,6 +44,7 @@ void SmartGuesser::learn(string reply){
  string SmartGuesser::convertIndexTOstr(int j){
     string s;
     string i=std::to_string(j);
+    //std::cout << "i" <<  i << std::endl;
     if (this->length==4)
     {
     if (j<10)
@@ -63,21 +72,38 @@ void SmartGuesser::learn(string reply){
     {
     s="0"+i;
     }
-    else 
+    else if (j<1000)
+    {
     s=i;
+    }
+    else 
+    s="??";
  }
 else if (this->length==2)
+  
   {
-  if (j<10)
+    if (j<10)
     {
     s="0"+i;
     }
-    else 
+    else if (j<100)
+    {
     s=i;
+    }
+    else
+    {
+    s="??";
+    }
   }
   else
   {
+      if (j<10)
       s=i;
+      else
+      {
+          s="??";
+      }
+      
   } 
     return s;
  }
