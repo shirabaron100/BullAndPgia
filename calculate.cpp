ef3dddff687a61ad782961c7fcb78786db80660b
shira baron <44798397+shirabaron100@users.noreply.github.com>
2019-05-07 14:59:31 +0300

    Add files via upload

--- calculate.cpp
+++ calculate.cpp
@@ -11,7 +11,7 @@ namespace bullpgia {
     int counter [9]={0};
 //     std::cout << "choice" << choice << std::endl;
 //     std::cout << "guess" << guess << std::endl;
-      for (int i=0;i<choice.length();i++)
+      for (int i=0;i<choice.length()&&i<guess.length();i++)
       {
       if (choice.at(i)==guess.at(i))
       {
@@ -23,7 +23,7 @@ namespace bullpgia {
         counter[(int)choice.at(i)-(int)'0']++;
       }
       }
-        for (int j=0;j<copyGuess.length();j++)
+        for (int j=0;j<copyGuess.length()&&j<choice.length();j++)
         {
           if (copyGuess.at(j)!='!'&&counter[(int)copyGuess.at(j)-(int)'0']>0)
           {
